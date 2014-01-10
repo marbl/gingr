8b57553e2f49bcf4aea6dbcda78783de71011c7a
Brian Ondov <ondovb@nbacc.net>
2014-01-10 11:40:00 -0500

    better search labels

--- src/SearchControl.cpp
+++ src/SearchControl.cpp
@@ -20,18 +20,41 @@ void SearchControl::initialize()
 {
 	QHBoxLayout * layoutSearch = new QHBoxLayout();
 	QVBoxLayout * layout = new QVBoxLayout();
+	QHBoxLayout * layoutTracks = new QHBoxLayout();
+	QHBoxLayout * layoutAnnotations = new QHBoxLayout();
+	
 	QPushButton * clearButton = new QPushButton("Clear");
 	QCheckBox * caseCheckBox = new QCheckBox("Match case");
 	lineEdit = new QLineEdit();
 	labelResultsTracks = new StatusLabel();
 	labelResultsAnnotations = new StatusLabel();
 	
+	QLabel * labelTracks = new QLabel(tr("tracks"));
+	QLabel * labelAnnotations = new QLabel(tr("annotations"));
+	
+	labelResultsAnnotations->setMinimumWidth(100);
+	labelResultsAnnotations->setMaximumWidth(100);
+	labelAnnotations->setMinimumWidth(100);
+	labelAnnotations->setMaximumWidth(100);
+	labelResultsTracks->setMinimumWidth(100);
+	labelResultsTracks->setMaximumWidth(100);
+	labelTracks->setMinimumWidth(100);
+	labelTracks->setMaximumWidth(100);
+	labelResultsTracks->setMinimumHeight(19);
+	labelResultsAnnotations->setMinimumHeight(19);
+	
 	layoutSearch->addWidget(lineEdit);
 	layoutSearch->addWidget(clearButton);
 	layout->addLayout(layoutSearch);
 	layout->addWidget(caseCheckBox);
-	layout->addWidget(labelResultsTracks);
-	layout->addWidget(labelResultsAnnotations);
+	layoutTracks->addWidget(labelResultsTracks);
+	layoutTracks->addWidget(labelTracks);
+	layoutTracks->setAlignment(Qt::AlignLeft);
+	layoutAnnotations->addWidget(labelResultsAnnotations);
+	layoutAnnotations->addWidget(labelAnnotations);
+	layoutAnnotations->setAlignment(Qt::AlignLeft);
+	layout->addLayout(layoutTracks);
+	layout->addLayout(layoutAnnotations);
 	
 	setLayout(layout);
 	
@@ -81,7 +104,7 @@ void SearchControl::resultsChangedTracks(int results)
 	}
 	else
 	{
-		labelResultsTracks->setText(QString("%1 tracks").arg(results));
+		labelResultsTracks->setText(QString("%1").arg(results));
 	}
 }
 
@@ -98,7 +121,7 @@ void SearchControl::resultsChangedAnnotations(int results)
 	}
 	else
 	{
-		labelResultsAnnotations->setText(QString("%1 annotations").arg(results));
+		labelResultsAnnotations->setText(QString("%1").arg(results));
 	}
 }
 
